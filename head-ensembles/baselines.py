f8c2b7ff374bf913797e53797ceb0d1733f556e4
limisiewicz <t.limisiewicz@gmail.com>
2020-04-27 15:21:27 +0200

    Baselines adapted to changed metric logic

--- head-ensembles/baselines.py
+++ head-ensembles/baselines.py
@@ -6,13 +6,17 @@ EN_PUD_TOKENS = "/net/projects/LSD/attention-analysis/acl2020/BertAA/BertAA-PUD-
 
 dependency_trees = Dependency(EN_PUD_CONLL, EN_PUD_TOKENS)
 
-uas_metric = UAS(dependency_trees.unlabeled_relations)
+uas_metric = UAS(dependency_trees)
 
 left_branching = [list(zip(range(1,len(toks)),range(0, len(toks)-1))) + [(0,-1)] for toks in dependency_trees.tokens]
 right_branching = [list(zip(range(0,len(toks)-1),range(1,len(toks)))) + [(len(toks)-1, -1)] for toks in dependency_trees.tokens]
-print("UAS left branching: ", uas_metric.calculate(range(1000), left_branching))
-print("UAS right branching: ", uas_metric.calculate(range(1000), right_branching))
 
+uas_metric.reset_state()
+uas_metric(range(1000), left_branching)
+print("UAS left branching: ", uas_metric.result())
+uas_metric.reset_state()
+uas_metric(range(1000), right_branching)
+print("UAS right branching: ", uas_metric.result())
 
 gr_right_branching = right_branching
 gr_left_branching = left_branching
@@ -23,13 +27,17 @@ for idx, root in enumerate(dependency_trees.roots):
 		gr_right_branching[idx].remove((root,root+1))
 		gr_right_branching[idx].append((root, -1))
 		gr_right_branching[idx].remove((last_tok_id , -1))
-		gr_right_branching[idx].append((last_tok_id , 0))
+		gr_right_branching[idx].append((root , 0))
 		
 	if root != 0:
 		gr_left_branching[idx].remove((root, root-1))
 		gr_left_branching[idx].append((root, -1))
 		gr_left_branching[idx].remove((0, -1))
-		gr_left_branching[idx].append((0, last_tok_id))
+		gr_left_branching[idx].append((root, last_tok_id))
 
-print("UAS left branching (with gold root): ", uas_metric.calculate(range(1000), gr_left_branching))
-print("UAS right branching (with gold root): ", uas_metric.calculate(range(1000), gr_right_branching))
+uas_metric.reset_state()
+uas_metric(range(1000), gr_left_branching)
+print("UAS left branching (with gold root): ", uas_metric.result())
+uas_metric.reset_state()
+uas_metric(range(1000), gr_right_branching)
+print("UAS right branching (with gold root): ", uas_metric.result())
